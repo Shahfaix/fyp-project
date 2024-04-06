1ab5b91f7972882ba907259f6cf4aca6911e7160
Shahfaix <11shahfaixbaig@gmail.com>
2024-04-07 02:51:24 +0500

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image';
+import Logo from "../../public/assests/Logo.png"
+
+
+const LoginPage: React.FC= () => {
+  return (
+   <div>
+    hi
+   </div>
+  )
+}
+
+export default LoginPage
